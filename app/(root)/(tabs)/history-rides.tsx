e1a72c408b8289acbc49d98e780d891a2b05b9f9
AbhishekSinghDev <singhabhishek7689@gmail.com>
2024-11-13 15:51:14 +0530

    add nativewind setup and tailwind setup. add a splash screen and a onboarding first screen

---
+++ app/(root)/(tabs)/history-rides.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const HistoryRides = () => {
+  return (
+    <View>
+      <Text>HistoryRides</Text>
+    </View>
+  );
+};
+
+export default HistoryRides;
